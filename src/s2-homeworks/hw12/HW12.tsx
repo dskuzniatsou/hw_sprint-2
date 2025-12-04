c0b70f1398c28da55240939a144b8e21e0fa870d
dskuzniatsou <sovyclub@gmail.com>
2025-12-04 22:53:20 +0300

    HW12

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -19,11 +19,12 @@ const themes = [
 ]
 
 const HW12 = () => {
-    // взять ид темы из редакса
-    const themeId = 1
+    const dispatch  = useDispatch()// взять ид темы из редакса
+    const themeId = useSelector((state: any) => state.theme.themeId)
+    // const themeId = 1
 
-    const change = (id: any) => { // дописать функцию
-
+    const change = (id: number) => { // дописать функцию
+        dispatch(changeThemeId(id))
     }
 
     useEffect(() => {
@@ -40,6 +41,9 @@ const HW12 = () => {
                 <SuperSelect
                     id={'hw12-select-theme'}
                     className={s.select}
+                    onChangeOption={change}
+                     value={themeId}
+                    options={themes}
                     // сделать переключение тем
 
                 />
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,22 @@
 const initState = {
     themeId: 1,
 }
+export type ThemeStateType = {
+    themeId: number
+}
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ChangeThemeActionType): ThemeStateType => { // fix any
     switch (action.type) {
-        // дописать
+        case 'SET_THEME_ID':
+            return { ...state, themeId: action.id }
+         // дописать
 
         default:
             return state
     }
 }
-
+export type ChangeThemeActionType = {
+    type: 'SET_THEME_ID'
+    id: number
+}
 export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
